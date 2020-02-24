56ffaebd3f3c2e80acd33a74d3f38edc9b05f3c0
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-24 06:52:49 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time1 = Time.new
+
+    if time1.inspect < 12:00:00
+      resp.write "Good Morning!"
+    else time1.inspect > 12:00:00
+      resp.write "Good Afternoon!"
+end
