ae4ebbe2776c31f81369c119d1c63cd2d1b6f8dd
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-24 07:03:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-  
+
 
     ran_num = Kernel.rand(1..24)
-    resp.write "#{ran_num}\n"
+    # resp.write "#{ran_num}\n"
     if ran_num < 12
       resp.write "Good Morning!"
     else ran_num > 12
